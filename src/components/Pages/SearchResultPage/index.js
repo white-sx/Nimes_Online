e9cf829ab046f0f6ac7f97dcbbd13126e0f70fb4
white-sx <faelsant10@gmail.com>
2023-01-03 19:08:07 -0300

    adding animations loading SearchResultPage

--- src/components/Pages/SearchResultPage/index.js
+++ src/components/Pages/SearchResultPage/index.js
@@ -6,6 +6,7 @@ import { Container, Ratio, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { GlobalContext } from "../../Api/GlobalContext";
 import useFetch from "../../Api/useFetch";
+import SpinnerComponent from "../../Spinner";
 
 function SearchResultPage() {
   const Global = React.useContext(GlobalContext);
@@ -21,55 +22,64 @@ function SearchResultPage() {
         },
       }
     );
-    if (data !== null) console.log(data);
+
   }, [Global.animeNameFormattedSearch]);
-  if (data !== null)
+  if (data !== null && Global.loading !== null)
     return (
       <>
-        <Container style={{ marginTop: "7rem" }}>
-          <Row>
-            <Col>
-              <h1 style={{ color: "#FAD82D" }}>Resultados para a busca:</h1>
-            </Col>
-          </Row>
-        </Container>
-        <Container style={{ marginTop: "3rem" }}>
-          <Row>
-            {data.map((data, index) => (
-              <Col key={index} xs={6} md={2}>
-                <Link
-                  to={"/anime"}
-                  style={{ textDecoration: "none", color: "#f2f2f2" }}
-                  onClick={function (e) {
-                    Global.setAnimeId(data.id);
-                    Global.setIdImage(data.category_image);
-                    Global.setAnimeTitle(data.category_name);
-                  }}
-                >
-                  <Card
-                    style={{
-                      width: "100%",
-                      height: "95%",
-                      backgroundColor: "#1f221f",
-                    }}
-                    variant="dark"
-                  >
-                    <Ratio aspectRatio="1x1">
-                      <Card.Img
-                        src={`https://cdn.appanimeplus.tk/img/${data.category_image}`}
-                      />
-                    </Ratio>
-                    <Card.Body>
-                      <Card.Title style={{ fontSize: ".9rem", width: "100%" }}>
-                        {data.category_name}
-                      </Card.Title>
-                    </Card.Body>
-                  </Card>
-                </Link>
-              </Col>
-            ))}
-          </Row>
-        </Container>
+        {Global.loading ? (
+          <SpinnerComponent />
+        ) : (
+          <>
+            {" "}
+            <Container style={{ marginTop: "7rem" }}>
+              <Row>
+                <Col>
+                  <h1 style={{ color: "#FAD82D" }}>Resultados para a busca:</h1>
+                </Col>
+              </Row>
+            </Container>
+            <Container style={{ marginTop: "3rem" }}>
+              <Row>
+                {data.map((data, index) => (
+                  <Col key={index} xs={6} md={2}>
+                    <Link
+                      to={"/anime"}
+                      style={{ textDecoration: "none", color: "#f2f2f2" }}
+                      onClick={function (e) {
+                        Global.setAnimeId(data.id);
+                        Global.setIdImage(data.category_image);
+                        Global.setAnimeTitle(data.category_name);
+                      }}
+                    >
+                      <Card
+                        style={{
+                          width: "100%",
+                          height: "95%",
+                          backgroundColor: "#1f221f",
+                        }}
+                        variant="dark"
+                      >
+                        <Ratio aspectRatio="1x1">
+                          <Card.Img
+                            src={`https://cdn.appanimeplus.tk/img/${data.category_image}`}
+                          />
+                        </Ratio>
+                        <Card.Body>
+                          <Card.Title
+                            style={{ fontSize: ".9rem", width: "100%" }}
+                          >
+                            {data.category_name}
+                          </Card.Title>
+                        </Card.Body>
+                      </Card>
+                    </Link>
+                  </Col>
+                ))}
+              </Row>
+            </Container>
+          </>
+        )}
       </>
     );
 }
