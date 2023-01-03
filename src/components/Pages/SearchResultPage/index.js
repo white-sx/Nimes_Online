b757cf4122c6b778f7c021a76d4afcf70a75729c
white-sx <faelsant10@gmail.com>
2023-01-03 19:14:17 -0300

    animations loading SearchResultPage correction

--- src/components/Pages/SearchResultPage/index.js
+++ src/components/Pages/SearchResultPage/index.js
@@ -22,64 +22,60 @@ function SearchResultPage() {
         },
       }
     );
-
   }, [Global.animeNameFormattedSearch]);
   if (data !== null && loading !== null)
     return (
       <>
-        {loading ? (
-          <SpinnerComponent />
-        ) : (
-          <>
-            {" "}
-            <Container style={{ marginTop: "7rem" }}>
-              <Row>
-                <Col>
-                  <h1 style={{ color: "#FAD82D" }}>Resultados para a busca:</h1>
-                </Col>
-              </Row>
-            </Container>
-            <Container style={{ marginTop: "3rem" }}>
-              <Row>
-                {data.map((data, index) => (
-                  <Col key={index} xs={6} md={2}>
-                    <Link
-                      to={"/anime"}
-                      style={{ textDecoration: "none", color: "#f2f2f2" }}
-                      onClick={function (e) {
-                        Global.setAnimeId(data.id);
-                        Global.setIdImage(data.category_image);
-                        Global.setAnimeTitle(data.category_name);
+        <Container style={{ marginTop: "7rem" }}>
+          <Row>
+            <Col>
+              <h1 style={{ color: "#FAD82D" }}>Resultados para a busca:</h1>
+            </Col>
+          </Row>
+        </Container>
+        <Container style={{ marginTop: "3rem" }}>
+          <Row>
+            {loading ? (
+              <SpinnerComponent />
+            ) : (
+              data.map((data, index) => (
+                <Col key={index} xs={6} md={2}>
+                  <Link
+                    to={"/anime"}
+                    style={{ textDecoration: "none", color: "#f2f2f2" }}
+                    onClick={function (e) {
+                      Global.setAnimeId(data.id);
+                      Global.setIdImage(data.category_image);
+                      Global.setAnimeTitle(data.category_name);
+                    }}
+                  >
+                    <Card
+                      style={{
+                        width: "100%",
+                        height: "95%",
+                        backgroundColor: "#1f221f",
                       }}
+                      variant="dark"
                     >
-                      <Card
-                        style={{
-                          width: "100%",
-                          height: "95%",
-                          backgroundColor: "#1f221f",
-                        }}
-                        variant="dark"
-                      >
-                        <Ratio aspectRatio="1x1">
-                          <Card.Img
-                            src={`https://cdn.appanimeplus.tk/img/${data.category_image}`}
-                          />
-                        </Ratio>
-                        <Card.Body>
-                          <Card.Title
-                            style={{ fontSize: ".9rem", width: "100%" }}
-                          >
-                            {data.category_name}
-                          </Card.Title>
-                        </Card.Body>
-                      </Card>
-                    </Link>
-                  </Col>
-                ))}
-              </Row>
-            </Container>
-          </>
-        )}
+                      <Ratio aspectRatio="1x1">
+                        <Card.Img
+                          src={`https://cdn.appanimeplus.tk/img/${data.category_image}`}
+                        />
+                      </Ratio>
+                      <Card.Body>
+                        <Card.Title
+                          style={{ fontSize: ".9rem", width: "100%" }}
+                        >
+                          {data.category_name}
+                        </Card.Title>
+                      </Card.Body>
+                    </Card>
+                  </Link>
+                </Col>
+              ))
+            )}
+          </Row>
+        </Container>
       </>
     );
 }
