c12c2a5521befdaea5f9a611a1977b65cfb8653e
white-sx <faelsant10@gmail.com>
2023-01-20 09:52:19 -0300

    optimizing AnimePage css

--- src/components/Pages/AnimePage/index.js
+++ src/components/Pages/AnimePage/index.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { GlobalContext } from "../../Api/GlobalContext";
 import useFetch from "../../Api/useFetch";
 import SpinnerComponent from "../../Spinner";
-
+import Styles from "./Custom.module.css";
 
 function AnimePage() {
   const { request, data, loading, error } = useFetch();
@@ -24,43 +24,17 @@ function AnimePage() {
   if (data !== null) {
     return (
       <>
-         <Container
-          style={{
-            marginTop: "8rem",
-            color: "#fff",
-            borderBottom: "3px solid #FAD82D",
-            paddingBottom: "1rem",
-          }}
-        >
-        
+        <Container className={Styles.containerMain}>
           <Row>
             <Col md="auto">
               <img src={`https://cdn.appanimeplus.tk/img/${Global.idImage}`} />
             </Col>
-            <Col style={{ marginTop: ".9rem" }}>
-              <h1 style={{ color: "#FAD82D" }}>{Global.animeTitle}</h1>
-              <span
-                style={{
-                  fontSize: " 0.875rem",
-                  color: " rgb(160, 160, 160)",
-                  display: "block",
-                  marginTop: "-10px",
-                }}
-              >
-                {Global.genres}
-              </span>
-              <div
-                style={{
-                  marginTop: "1rem",
-                  borderTop: "1px solid  rgb(76 76 76)",
-                  padding: "5px",
-                  fontSize: "1rem",
-                  lineHeight: "1.5rem",
-                }}
-              >
+            <Col className={Styles.animeTitle}>
+              <h1>{Global.animeTitle}</h1>
+              <span>{Global.genres}</span>
+              <div>
                 <p>{Global.description}</p>
               </div>
-              
             </Col>
           </Row>
         </Container>
@@ -70,14 +44,9 @@ function AnimePage() {
           ) : (
             data.map((item) => {
               return (
-
                 <Link
                   to={"/video"}
-                  style={{
-                    marginTop: "1rem",
-                    display: "block",
-                    textDecoration: "none",
-                  }}
+                  className={Styles.linkStyle}
                   key={item.video_id}
                   onClick={function (e) {
                     Global.setEpisodeId(item.video_id);
@@ -85,16 +54,14 @@ function AnimePage() {
                 >
                   <Card border="warning" bg="dark" text="light">
                     <Card.Body>
-                      <h4 style={{ fontSize: "1.2rem" }}>{item.title}</h4>
+                      <h4>{item.title}</h4>
                     </Card.Body>
                   </Card>
                 </Link>
-              
               );
             })
           )}
         </Container>
-        
       </>
     );
   }
