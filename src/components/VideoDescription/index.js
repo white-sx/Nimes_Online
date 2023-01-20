cae534c555566510625e7a45b75e583a693e0cde
white-sx <faelsant10@gmail.com>
2023-01-20 09:37:22 -0300

    optimizing VideoDescription css

--- src/components/VideoDescription/index.js
+++ src/components/VideoDescription/index.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import { GlobalContext } from "../Api/GlobalContext";
-import useFetch from "../Api/useFetch";
-import SpinnerComponent from "../Spinner";
+import styles from "./Custom.module.css";
 
 function VideoDescription({ visibility }) {
   const Global = React.useContext(GlobalContext);
@@ -11,44 +10,31 @@ function VideoDescription({ visibility }) {
     <>
       <Container
         style={{
-          color: "white",
-          marginTop: "1rem",
           display: `${visibility}`,
         }}
+        className={styles.ContainerMain}
       >
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <div
-            style={{
-              marginRight: "5px",
-              borderRight: "1px solid #ccc",
-              paddingRight: "6px",
-            }}
-          >
-            <span
-              style={{
-                color: "#FAD82D",
-                fontSize: "1rem",
-                textTransform: "capitalize",
-              }}
-            >
+        <div className={styles.splitContainer}>
+          <div className={styles.rightContainer}>
+            <span className={styles.rightContainerText}>
               {Global.animeTitle}
             </span>
           </div>
           <div>
-            <span style={{ fontSize: ".875rem" }}>
+            <span styles={{ fontSize: ".875rem" }}>
               {" "}
               {Global.animeReleaseYear}
             </span>
           </div>
         </div>
 
-        <h2 style={{ fontSize: "1.375rem" }}>{Global.currentEpisodeTitle}</h2>
-        <h3 style={{ fontSize: ".875rem", color: "#a0a0a0" }}>
+        <h2 >{Global.currentEpisodeTitle}</h2>
+        <h3>
           {" "}
           {Global.category_genres}
         </h3>
 
-        <p style={{ textAlign: "justify" }}>
+        <p>
           {` ${Global.description.substring(0, 424)}`}...
         </p>
       </Container>
